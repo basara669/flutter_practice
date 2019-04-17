156f3736959ba446e1901543e6d78610e3f748a4
basara669 <vengeance.669@gmail.com>
2019-04-17 20:37:09 +0900

    28まで

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
+//1つしか関数を使ってない時に使える
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  build(context) {
+//アンスコつけるとここでしか使えなくなる
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advance Food Taster');
+                    });
+                  },
+                  child: Text('Add Product'),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              )
+            ],
+          )),
     );
   }
 }
