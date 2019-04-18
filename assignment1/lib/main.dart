26c41302f60272b32d934466cbe786e3f7a81862
basara669 <vengeance.669@gmail.com>
2019-04-18 11:49:26 +0900

    課題1の追加

---
+++ assignment1/lib/main.dart
@@ -0,0 +1,17 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+//1つしか関数を使ってない時に使える
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(appBar: AppBar(title: Text('assignment'))));
+  }
+}
