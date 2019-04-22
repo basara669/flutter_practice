03f84ec4593cf4e542cda9ce75d30f885271eb24
basara669 <vengeance.669@gmail.com>
2019-04-22 13:02:21 +0900

    デバッグの書き方

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter/rendering.dart';
+
 import './product_manager.dart';
 
 //1つしか関数を使ってない時に使える
-void main() => runApp(MyApp());
+void main() {
+  debugPaintSizeEnabled = true;
+  debugPaintBaselinesEnabled = true;
+  debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   //アンスコつけるとここでしか使えなくなる{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowMaterialGrid: true,
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.deepOrange,
